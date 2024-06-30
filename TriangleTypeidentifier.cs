bc66047644c4808d78a830f1b0b9342847eea6f2
Martinselvis9 <99583829+Martinselvis9@users.noreply.github.com>
2024-06-30 01:36:06 +0000

    TRIANGLE TYPE IDENTIFIER

---
+++ TriangleTypeidentifier.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class TriangleTypeIdentifier
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("TYPES OF TRIANGLE");
+        Console.WriteLine("Enter the lengths of the three sides of the triangle separated by spaces:");
+        string[] parts = Console.ReadLine().Split(' ');
+        double a = double.Parse(parts[0]);
+        double b = double.Parse(parts[1]);
+        double c = double.Parse(parts[2]);
+
+        string triangleType = GetTriangleType(a, b, c);
+
+        Console.WriteLine($"Triangle Type: {triangleType}");
+    }
+
+    static string GetTriangleType(double a, double b, double c)
+    {
+        if (a + b <= c || a + c <= b || b + c <= a)
+        {
+            return "Invalid";
+        }
+        else if (a == b && b == c)
+        {
+            return "Equilateral";
+        }
+        else if (a == b || a == c || b == c)
+        {
+            return "Isosceles";
+        }
+        else
+        {
+            return "Scalene";
+        }
+    }
+}
