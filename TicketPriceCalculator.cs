d70d8a52e28eb5e2cb14c28e866e4e811b21c2cd
Martinselvis9 <99583829+Martinselvis9@users.noreply.github.com>
2024-06-30 01:32:26 +0000

    MY TICKET PRICE CALCULATOR

---
+++ TicketPriceCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class TicketPriceCalculator
+{
+    static void Main(string[] args)
+    {
+       Console.WriteLine("WELCOME TO TICKET PRICE CALCULATOR");
+        Console.WriteLine("Enter your age:");
+        int age = int.Parse(Console.ReadLine());
+
+        double price = CalculateTicketPrice(age);
+
+        Console.WriteLine($"Ticket Price: GHC{price}");
+    }
+
+    static double CalculateTicketPrice(int age)
+    {
+        if (age <= 12 || age >= 65)
+        {
+            return 7.00;
+        }
+        return 10.00;
+    }
+}
